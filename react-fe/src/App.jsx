218989461aa01338cad87baf19649b020439f1f6
Galen Gong <galengong@gmail.com>
2024-09-11 23:32:06 -0400

    Add text field and submit button to create users
    Make post request to backend with axios

--- react-fe/src/App.jsx
+++ react-fe/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [playerName, setPlayerName] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("http://localhost:3001/players", {
+        name: playerName,
+      });
+      alert(`Response: ${response.status}`);
+    } catch (error) {
+      console.error("There was an error submitting the username!", error);
+      alert("There was an error submitting the username!");
+    }
+  };
 
   return (
     <>
+      <h1>Enter Username</h1>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <TextField
+          variant="outlined"
+          value={playerName}
+          onChange={(e) => setPlayerName(e.target.value)}
+          fullWidth
+          margin="normal"
+          sx={{
+            backgroundColor: "white",
+          }}
+        />
+        <Button
+          variant="contained"
+          color="inherit"
+          onClick={handleSubmit}
+          sx={{
+            marginTop: 2,
+            width: "100%",
+          }}
+        >
+          Submit
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
